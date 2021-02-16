5344dc0d3a18a81f713c77ab0f13d4df3ce8e283
Michelle Lin <michelleamazinglin@gmail.com>
2021-02-15 23:48:08 -0500

    challenge 2

---
+++ Create with Code/Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    public float spawnInterval = 0.5f;
+    public bool canSpawn = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (canSpawn && Input.GetKeyDown(KeyCode.Space))
+        {
+            canSpawn = false;
+            Invoke(nameof(CanSpawnDog), spawnInterval);
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+
+    private void CanSpawnDog()
+    {
+        canSpawn = true;
+    }
+}
---
+++ Create with Code/Prototype2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+    private int ball;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 1.0f;
+
+    private float interval = 1.0f;
+    private float counter = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("randomInterval", startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        ball = Random.Range(0, ballPrefabs.Length);
+        Instantiate(ballPrefabs[ball], spawnPos, ballPrefabs[ball].transform.rotation);
+    }
+
+    void randomInterval(){
+        if(interval >= counter){
+            SpawnRandomBall();
+            counter = Random.Range(3,5);
+        }else{
+            interval++;
+        }
+    }
+
+}
